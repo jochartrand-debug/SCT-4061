c31abeb4ec323e3dbfcef51962ff6dc67791fdcf
jochartrand-debug <jo.chartrand@gmail.com>
2026-02-01 08:54:20 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,34 @@
+const CACHE_NAME = "unites-mesure-v53";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./sw.js",
+  "./assets/accueil.png",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./fonts/STIXTwoText-Italic.ttf"
+];
 
-// sw_fraction_bar_thinner.js
-const CACHE_NAME = "unites-mesure-v52";
-self.addEventListener("install", e => self.skipWaiting());
-self.addEventListener("activate", e => self.clients.claim());
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    (async () => {
+      const keys = await caches.keys();
+      await Promise.all(keys.map(k => (k === CACHE_NAME ? null : caches.delete(k))));
+      await self.clients.claim();
+    })()
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
