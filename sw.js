b6b7827cf55519d4e507c3266ec1ab1b4cef9306
jochartrand-debug <jo.chartrand@gmail.com>
2026-02-01 10:07:06 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,35 +1,34 @@
-const CACHE = "intervalles-cache-v43";
+const CACHE_NAME = "unites-mesure-v59";
 const ASSETS = [
   "./",
   "./index.html",
   "./styles.css",
   "./app.js",
   "./manifest.webmanifest",
-  "./data.json",  "./assets/accueil.png",
-  "./favicon.ico",
+  "./sw.js",
+  "./assets/accueil.png",
   "./icons/icon-192.png",
   "./icons/icon-512.png",
-  "./icons/apple-touch-icon.png",
-  "./icons/apple-touch-icon-167.png",
-  "./icons/apple-touch-icon-152.png",
-  "./icons/apple-touch-icon-120.png",
-  "./icons/favicon-32.png",
+  "./fonts/STIXTwoText-Italic.ttf"
 ];
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
 });
 
-self.addEventListener("activate", (e) => {
+self.addEventListener("activate", e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
+    (async () => {
+      const keys = await caches.keys();
+      await Promise.all(keys.map(k => (k === CACHE_NAME ? null : caches.delete(k))));
+      await self.clients.claim();
+    })()
   );
 });
 
-self.addEventListener("fetch", (e) => {
+self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
